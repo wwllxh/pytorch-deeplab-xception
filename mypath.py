d5963e9d5c04dfc911c3405e1e0ecfb933ef7ab7
wwllxh <97642952@qq.com>
2019-09-23 10:12:37 +0800

    Update mypath.py

--- mypath.py
+++ mypath.py
@@ -2,7 +2,7 @@ class Path(object):
     @staticmethod
     def db_root_dir(dataset):
         if dataset == 'pascal':
-            return '/path/to/datasets/VOCdevkit/VOC2012/'  # folder that contains VOCdevkit/.
+            return '/home/test-ww/Downloads/VOC2012/VOCdevkit/VOC2012'  # folder that contains VOCdevkit/.
         elif dataset == 'sbd':
             return '/path/to/datasets/benchmark_RELEASE/'  # folder that contains dataset/.
         elif dataset == 'cityscapes':
